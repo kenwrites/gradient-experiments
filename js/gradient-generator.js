30a72c7eb7ef23d8eb958399cc979bd8cebae54f
kenwrites <kenwrites@users.noreply.github.com>
2019-02-20 19:57:50 -0500

    Get color ranges, find color stops, and convert to RGB statements

--- js/gradient-generator.js
+++ js/gradient-generator.js
@@ -1,5 +1,6 @@
-
-var color_stops = {}
+var red_color_stops = {}
+var green_color_stops = {}
+var blue_color_stops = {}
 var gradient = ''
 var start_color_input = '0E4698'
 var start_colors = {}
@@ -7,7 +8,14 @@ var end_color_input = 'F4A8D9'
 var end_colors = {}
 var color_objects = {}
 var color_ranges = {}
-var intervals;
+var intervals = 10;
+var color_iterator = ["red", "green", "blue"]
+var all_color_stops = {
+    red: '',
+    green: '',
+    blue: ''
+}
+var rgb_color_stops = []
 
 function parse_rgb(hex_value) {
     let colors = {
@@ -22,11 +30,15 @@ function parse_rgb(hex_value) {
 }
 
 function hex_to_decimal(hex_num) {
-    let formatted_hex = '0x'
-    let decimal;
-    formatted_hex += hex_num
-    decimal = parseInt(formatted_hex)
+    let formatted_hex = '0x'+ hex_num
+    let decimal = parseInt(formatted_hex)
     return decimal
+}
+
+function color_obj_to_decimal(color_obj) {
+    for (var color in color_obj) {
+        color_obj[color] = hex_to_decimal(color_obj[color])
+    }
 }
 
 function get_color_ranges(colors) {
@@ -35,13 +47,66 @@ function get_color_ranges(colors) {
         green: '',
         blue: ''
     }
+    color_ranges.red = colors.end.red - colors.start.red
+    color_ranges.green = colors.end.green - colors.start.green
+    color_ranges.blue = colors.end.blue - colors.start.blue
     return color_ranges
 }
 
+function make_color_stops(color) {
+    let step
+    let color_stops = []
+    step = color_ranges[color] / intervals
+    // console.log("step = " + step)
+    for (let i = 0; i < intervals; i++) {
+        let color_stop
+        color_stop = start_colors[color] + (step * i)
+        // console.log("color_stop = " + color_stop)
+        color_stops.push(color_stop)
+        // console.log("color_stops = " + color_stops)
+    }
+    return color_stops
+}
+
+function write_rgb(stop) {
+    let rgb = 'rgb(' 
+    rgb += all_color_stops.red[stop] + ", "
+    rgb += all_color_stops.green[stop] + ", "
+    rgb += all_color_stops.blue[stop] + ")"
+    return rgb
+}
+
+// Extract individual color codes
+
 start_colors = parse_rgb(start_color_input)
 end_colors = parse_rgb(end_color_input)
+
+// Convert to decimal
+
+color_obj_to_decimal(start_colors)
+color_obj_to_decimal(end_colors)
+
+// Assign to color_objects, for input to get_color_ranges function
+
 color_objects.start = start_colors
 color_objects.end = end_colors
+
+// Get color ranges
+
 color_ranges = get_color_ranges(color_objects)
+
+// Calculate color stops
+
+for (var color in all_color_stops) {
+    all_color_stops[color] = make_color_stops(color)
+}
+
+// Convert to RGB statements
+
+for (let i = 0; i < intervals; i++) {
+    rgb_color_stops.push(write_rgb(i))
+}
+    
+// Write gradient statement 
 
 
