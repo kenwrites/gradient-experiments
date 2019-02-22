dfae4f43b40e9e46c488291441de01aac853e122
kenwrites <kenwrites@users.noreply.github.com>
2019-02-22 11:32:26 -0500

    Add and animate floating title box

--- js/carousel.js
+++ js/carousel.js
@@ -1,16 +1,23 @@
 const next_btn = document.querySelector('#carousel-next')
 const prev_btn = document.querySelector('#carousel-prev')
 const experiments = document.querySelectorAll('.ex-div')
-const title = document.querySelector('#experiment-title')
+const $title = $('#experiment-title')
 
 const last_slide = experiments.length - 1
 
 var slide = 0;
 
+function animate_title() {
+    let slide_title
+    slide_title = '"' + experiments[slide].title + '"'
+    $title.stop().hide().text(slide_title).fadeIn(500).delay(2000).fadeOut(500)
+}
+
 next_btn.addEventListener('click', () => {
     experiments[slide].style.display = "none"
     slide++;
     experiments[slide].style.display = "flex"
+    animate_title()
     if (slide === last_slide) {
         next_btn.style.display = "none"
     }
@@ -23,6 +30,7 @@ prev_btn.addEventListener('click', () => {
     experiments[slide].style.display = "none"
     slide--;
     experiments[slide].style.display = "flex"
+    animate_title()
     if (slide === 0) {
         prev_btn.style.display = "none"
     }
@@ -30,3 +38,7 @@ prev_btn.addEventListener('click', () => {
         next_btn.style.display = "block"
     }
 })
+
+// On page load
+
+animate_title()
