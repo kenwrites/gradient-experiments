8f4ef940b990f8687bb04314993ea18c92802276
kenwrites <kenwrites@users.noreply.github.com>
2019-02-22 16:42:43 -0500

    Let background expand when content overflows viewport.  Add Jellee typeface

--- js/carousel.js
+++ js/carousel.js
@@ -6,34 +6,49 @@ const faders = document.querySelectorAll('.fader')
 
 const last_slide = experiments.length - 1
 
-var slide = 0;
+var this_slide = 0;
+var prev_slide;
 
 function animate_title() {
     let slide_title
-    slide_title = '"' + experiments[slide].title + '"'
-    $title.stop().hide().delay(250).text(slide_title).fadeIn(500).delay(3000).fadeOut(500).stop()
+    slide_title = '"' + experiments[this_slide].title + '"'
+    $title.
+        stop().
+        hide().
+        delay(250).
+        text(slide_title).
+        fadeIn(500).
+        delay(3000).
+        fadeOut(500).
+        stop()
 }
 
-function fade_out() {
+function fade_out(slide) {
+    // console.log("fade in")
+    // console.log("this slide: " + this_slide)
     faders[slide].style.backgroundColor = "rgba(255, 255, 255, 1)"
 }
 
-function fade_in() {
+function fade_in(slide) {
+    // console.log("fade in")
+    // console.log("this slide: " + this_slide)
     faders[slide].style.backgroundColor = "rgba(255, 255, 255, 0)"
 }
 
 next_btn.addEventListener('click', () => {
-    fade_out()
+    prev_slide = this_slide;
+    fade_out(this_slide)
     window.setTimeout(() => {
-        experiments[slide].style.display = "none"
-        slide++;
-        experiments[slide].style.display = "flex"
-        fade_in()
+        experiments[this_slide].style.display = "none"
+        this_slide++;
+        experiments[this_slide].style.display = "flex"
+        fade_in(this_slide)
         animate_title()
-        if (slide === last_slide) {
+        if (this_slide === last_slide) {
             next_btn.style.display = "none"
+            faders[this_slide].style.display = "none"
         }
-        if (slide === 1) {
+        if (this_slide === 1) {
             prev_btn.style.display = "block"
         }    
     }, 250)
@@ -41,17 +56,17 @@ next_btn.addEventListener('click', () => {
 })
 
 prev_btn.addEventListener('click', () => {
-    fade_out()
+    fade_out(this_slide)
     window.setTimeout(() => {
-        experiments[slide].style.display = "none"
-        slide--;
-        experiments[slide].style.display = "flex"
-        fade_in()
+        experiments[this_slide].style.display = "none"
+        this_slide--;
+        experiments[this_slide].style.display = "flex"
+        fade_in(this_slide)
         animate_title()
-        if (slide === 0) {
+        if (this_slide === 0) {
             prev_btn.style.display = "none"
         }
-        if (slide === last_slide - 1) {
+        if (this_slide === last_slide - 1) {
             next_btn.style.display = "block"
         } 
     } )
@@ -60,5 +75,5 @@ prev_btn.addEventListener('click', () => {
 
 // On page load
 
-fade_in()
+fade_in(this_slide)
 animate_title()
